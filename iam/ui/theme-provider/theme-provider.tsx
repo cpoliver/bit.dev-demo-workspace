bf7681dee5e92e8005991e678d59a6aa2472ce24
Charles P. Oliver <charles.oliver@iamcloud.com>
2021-06-19 12:15:33 +0100

    fix dependencies?

--- iam/ui/theme-provider/theme-provider.tsx
+++ iam/ui/theme-provider/theme-provider.tsx
@@ -1,13 +1,16 @@
-import React from "react"
-import { ChakraProvider, extendTheme } from "@chakra-ui/react"
-import * as colors from "./DS_colors"
+import React from "react";
+import { ChakraProvider, extendTheme } from "@chakra-ui/react";
+// import emotion from "@emotion/react";
+import * as colors from "./DS_colors";
 
 const theme = extendTheme({
   colors: {
     ...colors,
   },
-})
+});
+
+// console.debug(emotion);
 
 export function ThemeProvider({ children }) {
-  return <ChakraProvider theme={theme}>{children}</ChakraProvider>
+  return <ChakraProvider theme={theme}>{children}</ChakraProvider>;
 }
